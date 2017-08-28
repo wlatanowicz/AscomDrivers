b173e97196382a6e0c3e61ae2d5293310306ea93
Wiktor Latanowicz <wiktor@latanowicz.com>
2017-08-28 21:29:20 +0200

    init commit - template driver

---
+++ NodeFocuser/NodeFocuser/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+// TODO - Add your authorship information here
+[assembly: AssemblyTitle("ASCOM.NodeFocuser.Focuser")]
+[assembly: AssemblyDescription("ASCOM Focuser driver for NodeFocuser")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("The ASCOM Initiative")]
+[assembly: AssemblyProduct("ASCOM Focuser driver for NodeFocuser")]
+[assembly: AssemblyCopyright("Copyright © 2017 The ASCOM Initiative")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("74dc21db-c84c-453d-a487-3ab18ca31cef")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+//
+// TODO - Set your driver's version here
+[assembly: AssemblyVersion("6.2.*")]
+[assembly: AssemblyFileVersion("6.2.0.0")]
